7eb46a0de5de3fadb80f53586bdfa6825857f063
Jairo Lozano <jairoglozano@gmail.com>
2018-11-12 23:41:49 -0700

    Listening to voice commands

---
+++ Assets/Actions.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Actions : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void FireAction()
+    {
+        Debug.Log("Fire");
+    }
+
+    public void DracarysAction()
+    {
+        Debug.Log("Dracarys");
+    }
+}
