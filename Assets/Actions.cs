f3a2ba7b048587876b9b20abd8d96c2191f16691
Jairo Lozano <jairoglozano@gmail.com>
2018-11-13 18:24:49 -0700

    Dragon scene finished

--- Assets/Actions.cs
+++ Assets/Actions.cs
@@ -6,6 +6,7 @@ public class Actions : MonoBehaviour {
 
     public GameObject flamesParticleEffect;
     private bool flamesActivated = false;
+    public SimpleObjectPool eggObjectPool;
 
     public AudioSource fireAudio;
 
@@ -16,8 +17,24 @@ public class Actions : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if (Input.GetKeyUp(KeyCode.U))
+        {
+            PutEgg();
+        }
 	}
+
+    public void PutEgg()
+    {
+        
+        
+        GameObject newEgg = eggObjectPool.GetObject();
+
+        // Reset egg acceleration
+        newEgg.GetComponent<Rigidbody>().velocity = Vector3.zero;
+
+        EggScript eggScript = newEgg.GetComponent<EggScript>();
+        eggScript.SetUp(eggObjectPool.gameObject.transform.position, eggObjectPool);
+    }
 
     public void FireAction()
     {
---
+++ Assets/EggScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EggScript : MonoBehaviour {
+
+    SimpleObjectPool parentObjectPool;
+
+	public void SetUp(Vector3 newPosition, SimpleObjectPool parentObjectPool)
+    {
+        this.parentObjectPool = parentObjectPool;
+        gameObject.transform.position = newPosition;
+        StartCoroutine(WaitToBeDestroyed());
+    }
+
+    IEnumerator WaitToBeDestroyed()
+    {
+        yield return new WaitForSeconds(15);
+
+        this.parentObjectPool.ReturnObject(gameObject);
+    }
+}
